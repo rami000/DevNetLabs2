e85e7d862dda23ee08e9f8318edc5b9b61768a0b
Rami <r-h_rami@live.nl>
2021-03-09 15:19:45 +0100

    updates

--- stage0.py
+++ stage0.py
@@ -5,14 +5,10 @@ dashboard = meraki.DashboardAPI(config['MERAKI_KEY'])
 
 def getOrganizationsData():
     request = dashboard.organizations.getOrganizations()
-    return request
-
-def printOrganizations():
-    request = dashboard.organizations.getOrganizations()
     print(request)
     return request
 
-printOrganizations()
+getOrganizationsData()
 
 
 
