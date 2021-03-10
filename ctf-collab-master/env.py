9343585dd44c47c5f1e0f0a6a94bb35655547b3c
Rami <s183266@student.dtu.dk>
2021-03-10 12:41:35 +0100

    Delete env.py

--- ctf-collab-master/env.py
+++
@@ -1,6 +0,0 @@
-config = {}
-
-config['WEBEX_BASE_URL'] = "https://webexapis.com"
-config['WEBEX_ACCESS_TOKEN'] = "<Replace me in setup>"
-config['PRODUCTION_ROOM'] = "<Replace me in stage 0>"
-config['TESTING_ROOM'] = "<Replace me in stage 1>"
