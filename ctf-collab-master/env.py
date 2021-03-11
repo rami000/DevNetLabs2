585e33855a5e0ad0ccce933d71d113ce2d2d72b6
Rami <r-h_rami@live.nl>
2021-03-11 09:13:32 +0100

    important removal

--- ctf-collab-master/env.py
+++
@@ -1,7 +0,0 @@
-config = {}
-
-config['WEBEX_BASE_URL'] = "https://webexapis.com"
-config['WEBEX_ACCESS_TOKEN'] = "MDMzOTZkYmQtYTkyZC00YWUwLTllMGItZWM3YzQyNWJlYWY4NjgzOWRjN2QtODdh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-config['PRODUCTION_ROOM'] = "Y2lzY29zcGFyazovL3VzL1JPT00vM2ZmOGU3ZjAtODAwOS0xMWViLTg4NjUtNTE2OGI4NmU0NjA4"
-config['TESTING_ROOM'] = "<Replace me in stage 1>"
-
