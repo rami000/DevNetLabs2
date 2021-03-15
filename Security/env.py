169dbea0821f814ecd40a4bf46ddbda3afbcf3d4
Rami <r-h_rami@live.nl>
2021-03-15 15:24:01 +0100

    initial commit

---
+++ Security/env.py
@@ -0,0 +1,32 @@
+#Environmental Information for this Event
+
+#AMP
+AMP = {
+    "host": "api.eu.amp.cisco.com",
+    "client_id": "a941000ac4eaf6b05f51",
+    "api_key": "56ab38c3-f9dd-4d0c-8ba6-e993ba843a1e"
+    }
+
+# ThreatGrid
+THREATGRID = {
+    "host": "panacea.threatgrid.eu",
+    "api_key": "un8tah0hmnggaj4isoad7tl3o1"
+    }
+
+UMBRELLA = {
+    "en_url": "https://s-platform.api.opendns.com/1.0/",
+    "inv_url": "https://investigate.api.umbrella.com", 
+    "inv_token": "a3a5258b-c898-403d-8d01-6352a6360cd0",
+    "en_key": "87d72fbf-b488-445b-9b12-1c62b66827b4"
+    }
+
+THREATRESPONSE = {
+    "ctr_host": "visibility.eu.amp.cisco.com",
+    "ctr_client_id": "client-dbe00db4-94f7-4fd0-8137-1adb5d6d08c4",
+    "ctr_client_pwd": "_KJQWSdPu_btREeyvC6DxrXxQvAhFsfjBjTYyq7R6Yj5sgYk2bFJ0A"
+    }
+
+WEBEX = {
+    "room_id": "<insert room_id>",
+    "personal_access_token": "<insert personal_access_token>"
+    }
